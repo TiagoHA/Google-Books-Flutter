7cb0e352626c619dfe6e9f7e47a73d2f65c766c4
Tiago Almeida <tiagu.almeida@gmail.com>
2018-12-28 00:48:09 -0200

    add homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:google_books/src/blocs/application_bloc.dart';
+import 'package:google_books/src/providers/bloc_provider.dart';
 import 'package:google_books/src/resources/repository.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+    BlocProvider<ApplicationBloc>(bloc: ApplicationBloc(), child: MyApp()));
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // debugPrintRebuildDirtyWidgets = true;
     return MaterialApp(
       title: 'Google Books',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme:
+          ThemeData(primarySwatch: Colors.blue, backgroundColor: Colors.yellow),
       home: MyHomePage(),
     );
   }
@@ -28,47 +31,61 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   final repository = Repository();
 
   void _incrementCounter() {
-    repository
-        .fetchAllBooks(search: 'Harry Potter')
-        .then((data) {
+    repository.fetchAllBooks(search: 'Harry Potter').then((data) {
       print('DATA FINAL: ');
       print(json.encode(data));
-    });
-
-    setState(() {
-      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Google Books Home Page'),
-      ),
+      backgroundColor: Colors.yellow,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'GOOGLE BOOKS',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontFamily: 'Roboto-Bold',
+                    fontSize: 26),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                autofocus: false,
+                style: TextStyle(color: Colors.white, decorationColor: Colors.white),
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Which books do you want to search?'),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: OutlineButton(
+                      child: Text('Search'),
+                      onPressed: () {},
+                      padding: EdgeInsets.all(15),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
